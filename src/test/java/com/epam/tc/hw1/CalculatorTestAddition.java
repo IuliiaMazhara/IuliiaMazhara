a1cc3d52a0448d0bc68b5bcfb7fc9a111af3ebed
Iuliia_Mazhara <tm41mazhara@gmail.com>
2022-12-21 15:11:37 +0100

    Since the calculator is no longer there, commented test for the HM1

--- src/test/java/com/epam/tc/hw1/CalculatorTestAddition.java
+++ src/test/java/com/epam/tc/hw1/CalculatorTestAddition.java
@@ -1,21 +1,21 @@
-package com.epam.tc.hw1;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-public class CalculatorTestAddition extends TestingArithmeticOperations {
-
-    @DataProvider(name = "data-provider-for-addition")
-    public Object[][] dataSetForAddition() {
-        return new Object[][]{{1, 2, 3}, {-2, -4, -6}, {-121, -4, -125}, {0, 0, 0}};
-    }
-
-    @Test(dataProvider = "data-provider-for-addition", groups = {"subAdd"})
-    public void sumTest(long a, long b, long expectedAddition) {
-        long actualAddition = calculator.sum(a, b);
-        assertThat(actualAddition).isEqualTo(expectedAddition);
-    }
-
-}
+//package com.epam.tc.hw1;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import org.testng.annotations.DataProvider;
+//import org.testng.annotations.Test;
+//
+//public class CalculatorTestAddition extends TestingArithmeticOperations {
+//
+//    @DataProvider(name = "data-provider-for-addition")
+//    public Object[][] dataSetForAddition() {
+//        return new Object[][]{{1, 2, 3}, {-2, -4, -6}, {-121, -4, -125}, {0, 0, 0}};
+//    }
+//
+//    @Test(dataProvider = "data-provider-for-addition", groups = {"subAdd"})
+//    public void sumTest(long a, long b, long expectedAddition) {
+//        long actualAddition = calculator.sum(a, b);
+//        assertThat(actualAddition).isEqualTo(expectedAddition);
+//    }
+//
+//}
