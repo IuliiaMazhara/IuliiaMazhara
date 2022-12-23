809bc20c7ceedd1c36121d45287eb9b8831dad9b
Iuliia_Mazhara <tm41mazhara@gmail.com>
2022-12-23 14:56:15 +0100

    Added options to Chrome Driver

--- src/test/java/com/epam/tc/hw3/utils/DriverManager.java
+++ src/test/java/com/epam/tc/hw3/utils/DriverManager.java
@@ -3,23 +3,30 @@ package com.epam.tc.hw3.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
 
 
 public class DriverManager {
 
     private void setupChromeDriver() {
-        WebDriverManager.chromedriver()
-                .setup();
+        WebDriverManager.chromedriver().setup();
+
+
+
     }
 
     public WebDriver setupDriver() {
         setupChromeDriver();
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--incognito");
+        WebDriver webDriver = new ChromeDriver(options);
         webDriver.manage().window().maximize();
         webDriver.get("https://jdi-testing.github.io/jdi-light/index.html");
         return webDriver;
     }
+
 
 
 }
