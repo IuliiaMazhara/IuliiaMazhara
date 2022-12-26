aa4abffa637b44dd73e8d8d33b05f9170a82610a
Iuliia_Mazhara <tm41mazhara@gmail.com>
2022-12-26 01:57:44 +0100

    HM4: Remove  import

--- src/test/java/com/epam/tc/hw4/TestEx1.java
+++ src/test/java/com/epam/tc/hw4/TestEx1.java
@@ -1,22 +1,12 @@
 package com.epam.tc.hw4;
 
 
-import com.epam.tc.hw3.pages.PageObjectHome;
-import com.epam.tc.hw3.pages.PageObjectLoginPage;
 import com.epam.tc.hw4.utils.ActionStep;
 import com.epam.tc.hw4.utils.AssertStep;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
-import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import org.assertj.core.api.SoftAssertions;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 
